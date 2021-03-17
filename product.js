41ab0422bceca2abc66550dddf6cc003771cf258
Leo Wang <56250432+F74064062@users.noreply.github.com>
2021-03-17 19:50:09 +0800

    Add files via upload
    complete RWD version of hw1

---
+++ product.js
@@ -0,0 +1,29 @@
+function to_bench(){
+	/*window.scrollTo({top:0*vw, behavior:'smooth'});*/
+	document.getElementById("bench").scrollIntoView({block:'start', behavior:'smooth'});
+}
+function to_chair(){
+	/*window.scrollTo({top:0*vw, behavior:'smooth'});*/
+	document.getElementById("chair").scrollIntoView({block:'start', behavior:'smooth'});
+}
+function to_chair2(){
+	/*window.scrollTo({top:0*vw, behavior:'smooth'});*/
+	document.getElementById("chair2").scrollIntoView({block:'start', behavior:'smooth'});
+}
+function to_chair3(){
+	/*window.scrollTo({top:0*vw, behavior:'smooth'});*/
+	document.getElementById("chair3").scrollIntoView({block:'start', behavior:'smooth'});
+}
+function to_light(){
+	/*window.scrollTo({top:0*vw, behavior:'smooth'});*/
+	document.getElementById("light").scrollIntoView({block:'start', behavior:'smooth'});
+}
+function to_painting(){
+	/*window.scrollTo({top:0*vw, behavior:'smooth'});*/
+	document.getElementById("painting").scrollIntoView({block:'start', behavior:'smooth'});
+}
+function to_pillow(){
+	/*window.scrollTo({top:0*vw, behavior:'smooth'});*/
+	document.getElementById("pillow").scrollIntoView({block:'start', behavior:'smooth'});
+}
+
